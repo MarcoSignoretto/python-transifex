d2041c8801a162882c2c89f8248e16038311d1ba
Marco Signoretto <marco.signoretto@careem.com>
2020-08-09 12:01:00 +0200

    rtesfg

--- setup.py
+++ setup.py
@@ -1,33 +1,35 @@
 import os
-from setuptools import setup, find_packages
 import sys
+
+from setuptools import find_packages
+from setuptools import setup
 
 from transifex import VERSION
 
 if sys.argv[-1] == 'publish-to-pypi':
-    os.system("python setup.py sdist upload -r pypi")
-    os.system("git tag -a %s -m 'version %s'" % (VERSION, VERSION))
-    os.system("git push --tags")
-    sys.exit()
+	os.system("python setup.py sdist upload -r pypi")
+	os.system("git tag -a %s -m 'version %s'" % (VERSION, VERSION))
+	os.system("git push --tags")
+	sys.exit()
 
 setup(
-    name='python-transifex',
-    version=VERSION,
-    description='A Python API to the Transifex translation service (www.transifex.com).',
-    author='Craig Blaszczyk',
-    author_email='masterjakul@gmail.com',
-    url='https://github.com/jakul/python-transifex',
-    license='BSD',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=['requests>=0.12.1'],
-    classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
+	name='python-transifex',
+	version=VERSION,
+	description='A Python API to the Transifex translation service (www.transifex.com).',
+	author='Craig Blaszczyk',
+	author_email='masterjakul@gmail.com',
+	url='https://github.com/jakul/python-transifex',
+	license='BSD',
+	packages=find_packages(),
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=['requests>=0.12.1'],
+	classifiers=[
+		'Environment :: Web Environment',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: BSD License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+	],
 )
